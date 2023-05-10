d364c9701659cd8ed9d4c34cadcccdf0136408f2
tkj <kahjun.teh@thetasp.com>
2023-05-10 10:04:22 +0800

    Track library installed at ANTELOPE-765

--- dev/builder/build-config.js
+++ dev/builder/build-config.js
@@ -35,67 +35,71 @@ var CKBUILDER_CONFIG = {
 		'tests'
 	],
 	plugins: {
-		a11yhelp: 1,
-		about: 1,
-		basicstyles: 1,
+		a11yhelp: 1, // need
+		about: 1, // need
+		basicstyles: 1, // need
 		bidi: 1,
-		blockquote: 1,
-		clipboard: 1,
+		blockquote: 1, // need
+		clipboard: 1, // need
 		colorbutton: 1,
 		colordialog: 1,
 		copyformatting: 1,
-		contextmenu: 1,
+		contextmenu: 1, // need
 		dialogadvtab: 1,
 		div: 1,
-		elementspath: 1,
-		enterkey: 1,
-		entities: 1,
-		filebrowser: 1,
+		elementspath: 1, // need
+		enterkey: 1, // need
+		entities: 1, // need
+		filebrowser: 1, // need
 		find: 1,
 		flash: 1,
-		floatingspace: 1,
+		floatingspace: 1, // need
 		font: 1,
-		format: 1,
+		format: 1, // need
 		forms: 1,
-		horizontalrule: 1,
-		htmlwriter: 1,
+		horizontalrule: 1, // need
+		htmlwriter: 1, // need
 		iframe: 1,
-		image: 1,
-		indentlist: 1,
+		image: 1, // need
+		indentlist: 1, // need
 		indentblock: 1,
 		justify: 1,
 		language: 1,
-		link: 1,
-		list: 1,
+		link: 1, // need
+		list: 1, // need
 		liststyle: 1,
-		magicline: 1,
-		maximize: 1,
+		magicline: 1, // need
+		maximize: 1, // need
 		newpage: 1,
 		pagebreak: 1,
 		pastefromlibreoffice: 1,
-		pastefromword: 1,
-		pastetext: 1,
+		pastefromword: 1, // need
+		pastetext: 1, // need
 		editorplaceholder: 1,
 		preview: 1,
 		print: 1,
-		removeformat: 1,
-		resize: 1,
+		removeformat: 1, // need
+		resize: 1, // need
 		save: 1,
 		selectall: 1,
 		showblocks: 1,
-		showborders: 1,
+		showborders: 1, // need
 		smiley: 1,
-		sourcearea: 1,
-		specialchar: 1,
-		stylescombo: 1,
-		tab: 1,
-		table: 1,
-		tableselection: 1,
-		tabletools: 1,
+		sourcearea: 1, // need
+		specialchar: 1, // need
+		stylescombo: 1, // need
+		tab: 1, // need
+		table: 1, // need
+		tableselection: 1, // need
+		tabletools: 1, // need
 		templates: 1,
-		toolbar: 1,
-		undo: 1,
-		uploadimage: 1,
-		wysiwygarea: 1
+		toolbar: 1, // need
+		undo: 1, // need
+		uploadimage: 1, // need
+		wysiwygarea: 1, // need
+
+		// to check
+		// wsc : 1, // missing, EOL at 2021
+		// scayt : 1, // missing, already builtin
 	}
 };
